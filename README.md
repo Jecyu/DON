a041e49945c4594b13e6b896ab0c86f954de7388
Jecyu Lin <jecyu.lin@gmail.com>
2017-12-17 13:57:16 +0800

    初始化开发环境

---
+++ README.md
@@ -0,0 +1,3 @@
+花森动漫门户网站，你最爱的动漫网站。
+===============
+运用HTML5、CSS3、JavaScript、JQuery、Echarts等技术实现，
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Jecyu
+ * @Date: 2017-12-17 1:01:38 pm 
+ * @Modified By: JeCyu 
+ * @Last Modified time: 2017-12-17 1:01:50 pm 
+ */
+
+let a = 1;
